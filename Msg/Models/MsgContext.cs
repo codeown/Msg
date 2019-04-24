48a77f609eb60ffcb5b740fa0db4de5e26dc706b
codemass <worklette@yandex.ru>
2019-04-24 23:16:34 +0300

    Добавлен контекст данных

---
+++ Msg/Models/MsgContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Msg.Models
+{
+    public class MsgContext : DbContext
+    {
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<SendMessage> SendMessages { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
