9323edf6411aea94e28048197c1278db7210e993
codemass <worklette@yandex.ru>
2019-04-24 23:05:52 +0300

    Добавлены модели

---
+++ Msg/Models/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Msg.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Boby { get; set; }
+    }
+}
---
+++ Msg/Models/SendMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Msg.Models
+{
+    public class SendMessage
+    {
+        public int Id { get; set; } //Id сообщения
+        public DateTime DateTime { get; set; }
+        public int FromUser { get; set; }
+        public int ToUser { get; set; }
+    }
+}
